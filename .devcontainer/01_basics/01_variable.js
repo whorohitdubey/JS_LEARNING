bb86234e721111d0b50112f444458e263afcfe60
whorohitdubey <88968882+whorohitdubey@users.noreply.github.com>
2023-12-27 16:11:57 +0000

    variable added

---
+++ .devcontainer/01_basics/01_variable.js
@@ -0,0 +1,15 @@
+const accountID= 143555
+let accountEmail= "rohit20140004@gmail.com"
+var accountPassword="12345"
+accountCity= "jaipur";
+let accountState;
+
+
+accountEmail= "rohit@gmail.com"
+var accountPassword="345"
+accountCity= "ipur"
+
+console.log(accountID);
+console.table([accountID,accountEmail,accountPassword,accountCity,accountState])
+
+//note: Do not use var anymore because of issue in block scope and functional scope//
